7660c477cef046067dd3d7ddb4e869a4c010dafe
filland <kubalexei@gmail.com>
2020-03-07 03:12:16 +0300

    feat: impl the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const whitespace = "**********";
+    expr = expr.split(whitespace).join(" ");
+    let result = [];
+    
+    for (let i = 0; i < expr.length; i++) {
+        if(expr.charAt(i) !== ' ') {
+            let letter = findLetter(expr.slice(i, 10+i));
+            i+=9;
+            result.push(letter);
+        } else {
+            result.push(" ");
+            continue;
+        }
+    }
+    return result.join("");
+}
+
+function findLetter(code) {
+    const c = {
+        "10": ".",
+        "11": "-"
+    } 
+    let unpaddedLetter = code;
+    for (let i = 0; i < code.length; i++) {
+        const letter = code.charAt(i);
+        if(letter === '0') {
+            unpaddedLetter = code.substring(1+i, code.length);
+        } else {
+            break;
+        }
+    }
+
+    let signs = [];
+    for (let y = 0; y < unpaddedLetter.length; y++) {
+        if(y%2===0) {
+            signs.push(c[unpaddedLetter.substring(y, y+2)]);
+        }
+    }
+
+    return MORSE_TABLE[signs.join("")];
 }
 
 module.exports = {
